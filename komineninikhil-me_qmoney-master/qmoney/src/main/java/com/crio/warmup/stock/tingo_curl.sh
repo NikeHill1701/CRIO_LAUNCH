19a413685ba02b10225c35138ca20cc95af510f2
NikeHill1701 <nikhilk.kommineni@gmail.com>
2020-09-07 23:24:32 +0530

    Done!

---
+++ komineninikhil-me_qmoney-master/qmoney/src/main/java/com/crio/warmup/stock/tingo_curl.sh
@@ -0,0 +1,3 @@
+curl -X GET https://api.tiingo.com/tiingo/daily/aapl/prices?token=910fefa416692213ec155cccafe2c9c23ef63594
+
+curl -X GET https://api.tiingo.com/tiingo/daily/googl/prices?token=910fefa416692213ec155cccafe2c9c23ef63594
